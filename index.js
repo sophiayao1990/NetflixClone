9d52fb389689cfa45e7e655e319d31ee10cfc9c5
Lianhui Yao <lianhuiyao@Lianhuis-MacBook-Pro.local>
2023-02-16 22:31:41 -0500

    working on the main part

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// document.getElementById("hambuger-menu").addEventListener("click", () => {
+//   const menu = document.getElementById("menu");
+//   if (menu.style.display === "none") {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// });
+
 document.getElementById("hambuger-menu").addEventListener("click", () => {
-  const menu = document.getElementById("menu");
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
+  document.getElementById("menu").classList.toggle("show-menu");
 });
